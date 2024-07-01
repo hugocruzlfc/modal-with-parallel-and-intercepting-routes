65e66b7eba5ad3be784d5657f70d9fc920a952ec
hugocruzlfc <hugocruzlfc@gmail.com>
2024-07-01 19:46:43 +0200

    feat: add project components and config

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="border-b-2 px-8 py-4 mb-4 container mx-auto">
+      <Link href="/">
+        <h1 className="text-3xl text-center">🚀 The Star Wars Feed  ★</h1>
+      </Link>
+    </header>
+  );
+}
